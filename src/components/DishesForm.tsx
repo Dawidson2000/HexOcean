35bd4229bd0040dc22509e7ea375d47e0c9e6694
Dawid Kułaga <dawidson.kiano@gmail.com>
2022-02-28 17:38:35 +0100

    Added React Final Form

--- src/components/DishesForm.tsx
+++ src/components/DishesForm.tsx
@@ -5,7 +5,9 @@ import InputWrapper from "../UI/InputWrapper";
 import Select from "../UI/Select";
 import SubmitButton from "../UI/SubmitButton";
 
-const Form = styled.form`
+import { Form, Field } from 'react-final-form';
+
+const StyledForm = styled.form`
     max-width: 500px;
     margin: 0 auto;
     margin-top: 100px;
@@ -18,26 +20,47 @@ const Form = styled.form`
 `;
 
 const Dishesform: FC = () => {
+    const onSubmit = async (values: any) => {
+        console.log(values);
+    };
+
     return (
-        <Form>
-            <InputWrapper>
-                <label htmlFor='name'>Name</label>
-                <Input id='name' type='text'/>
-            </InputWrapper>
-            <InputWrapper>
-                <label htmlFor='name'>Preparation time</label>
-                <Input id='name' type='time' step='1'/>
-            </InputWrapper>
-            <InputWrapper>
-                <label htmlFor='type'>Type</label>
-                <Select id='type'>
-                    <option value={'pizza'}>pizza</option>
-                    <option value={'soup'}>soup</option>
-                    <option value={'sandwich'}>sandwich</option>
-                </Select>
-            </InputWrapper>
-            <SubmitButton type='submit'>Submit</SubmitButton>
-        </Form>
+        <Form
+            onSubmit={onSubmit}
+            render={({ handleSubmit }) => (
+                <StyledForm onSubmit={handleSubmit}>
+                    <InputWrapper>
+                        <label>First Name</label>
+                        <Field name='name'>
+                            {props => (
+                                <Input id='name' type='text' onChange={props.input.onChange} value={props.input.value}/>
+                            )}
+                        </Field>
+                    </InputWrapper>
+                    <InputWrapper>
+                        <label>Prepaparation Time</label>
+                        <Field name='preparation_time'>
+                            {props => (
+                                <Input id='preparation_time' type='time' step='1' onChange={props.input.onChange} value={props.input.value}/>
+                            )}
+                        </Field>
+                    </InputWrapper>
+                    <InputWrapper>
+                        <label>Type</label>
+                        <Field name='type'>
+                            {props => (
+                                <Select id='type' name='type' value={props.input.value} onChange={props.input.onChange}>
+                                    <option value={'pizza'}>pizza</option>
+                                    <option value={'soup'}>soup</option>
+                                    <option value={'sandwich'}>sandwich</option>
+                                </Select>
+                            )}
+                        </Field>
+                    </InputWrapper>
+                    <SubmitButton type='submit'>Submit</SubmitButton>
+                </StyledForm>
+            )}
+        />
     )
 };
 
