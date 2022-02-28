b8a2c9c6dde6fc5c349270e7cd67610edafc916a
Dawid Kułaga <dawidson.kiano@gmail.com>
2022-02-28 15:59:19 +0100

    Added submit button

---
+++ src/UI/SubmitButton.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { Colors } from '../styledHelpers/Colors';
+
+const SubmitButton = styled.button`
+    border: none;
+    padding: 5px;
+    outline: none;
+    font-family: inherit;
+    background-color: ${Colors.mainThemeColor};
+    color: white;
+    cursor: pointer;
+    font-size: 0.9rem;
+`;
+
+export default SubmitButton;
