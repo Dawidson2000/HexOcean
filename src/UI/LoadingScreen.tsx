fc5316087c60237a625a8e6fa8d4f555112daef1
Dawid Kułaga <dawidson.kiano@gmail.com>
2022-03-01 16:58:32 +0100

    Added loadingScreen

---
+++ src/UI/LoadingScreen.tsx
@@ -0,0 +1,43 @@
+import { FC } from "react";
+import styled from "styled-components";
+import { Colors } from "../styledHelpers/Colors";
+
+const Dots = styled.div`
+    width: 100%;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: center;
+    & div {
+        width: 0.8em;
+        height: 0.8em;
+        border-radius: 50%;
+        background-color: ${Colors.mainThemeColor};
+        animation: fade 0.8s ease-in-out alternate infinite;
+        margin: 0 5px;
+    }
+    & div:nth-of-type(1) {
+        animation-delay: -0.4s;
+    }   
+    & div:nth-of-type(2) {
+        animation-delay: -0.2s;
+    }
+    @keyframes fade {
+        from {
+            opacity: 1;
+        } 
+        to {
+            opacity: 0;
+        }
+    }
+`;
+
+export const LoadingScreen: FC = () => {
+    return (
+        <Dots>
+            <div></div>
+            <div></div>
+            <div></div>
+        </Dots>
+    )
+};
